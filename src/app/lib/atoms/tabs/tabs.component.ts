c615f13367f8a31ea7a18ed676514c6f38ead575
lilithovhannisyan <lilit.hovhannisyan.armeni@gmail.com>
2019-08-02 19:16:34 +0400

    styling

--- src/app/lib/atoms/tabs/tabs.component.ts
+++ src/app/lib/atoms/tabs/tabs.component.ts
@@ -3,6 +3,7 @@ import { Component, Input} from '@angular/core';
 @Component ({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
+    styleUrls: ['./tabs.component.less']
 })
 
 export class TabsComponent {
